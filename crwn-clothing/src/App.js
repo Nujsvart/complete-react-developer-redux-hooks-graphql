4e2aa1ed73b439961270d9bd2d74f54131e2393b
Nujsvart <kampfar@gmail.com>
2023-01-24 03:00:49 +0300

    category directory

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,40 +1,9 @@
-import "./categories.styles.scss";
-
-const categories = [
-  {
-    id: 1,
-    title: "Hats",
-  },
-  {
-    id: 2,
-    title: "Jackets",
-  },
-  {
-    id: 3,
-    title: "Sneakers",
-  },
-  {
-    id: 4,
-    title: "Womens",
-  },
-  {
-    id: 5,
-    title: "Mens",
-  },
-];
+import Categories from "./components/Categories/Categories";
 
 const App = () => {
   return (
-    <div className="categories-container">
-      {categories.map(category => (
-        <div key={category.id} className="category-container">
-          <div className="background-image" />
-          <div className="category-body-container">
-            <h2>{category.title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-      ))}
+    <div>
+      <Categories />
     </div>
   );
 };
