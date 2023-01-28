be9f17c87255c88798c4934e8d2f78304f77bdab
Nujsvart <kampfar@gmail.com>
2023-01-29 00:45:29 +0300

    sign in with redirect

--- crwn-clothing/src/routes/SignIn/SignIn.js
+++ crwn-clothing/src/routes/SignIn/SignIn.js
@@ -1,9 +1,26 @@
+import { useEffect } from "react";
+
+import { getRedirectResult } from "firebase/auth";
+
 import {
+  auth,
   signInWithGooglePopup,
+  signInWithGoogleRedirect,
   createUserDocumentFromAuth,
 } from "../../utils/firebase";
 
 const SignIn = () => {
+  useEffect(() => {
+    const handleRedirect = async () => {
+      const res = await getRedirectResult(auth);
+
+      if (res) {
+        const user = await createUserDocumentFromAuth(res.user);
+      }
+    };
+    handleRedirect();
+  }, []);
+
   const logGoogleUser = async () => {
     const res = await signInWithGooglePopup();
     console.log(res);
@@ -17,6 +34,9 @@ const SignIn = () => {
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign in With Google Popup</button>
+      <button onClick={signInWithGoogleRedirect}>
+        Sign in With Google Redirect
+      </button>
     </div>
   );
 };
